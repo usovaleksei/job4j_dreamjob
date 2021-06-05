6454c4e1b7834da5113f0ac3b0709b531d07b6ed
Usov Aleksei <usalekse@gmail.com>
2021-06-05 18:24:07 +0300

    11.3.1. Servlet. Web.xml [#6866]

---
+++ src/main/java/ru/job4j/dream/servlet/PostServlet.java
@@ -0,0 +1,21 @@
+package ru.job4j.dream.servlet;
+
+import ru.job4j.dream.model.Post;
+import ru.job4j.dream.store.Store;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class PostServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        Store.instOf().save(new Post(0, req.getParameter("name"), req.getParameter("description"), LocalDateTime.now()));
+        resp.sendRedirect(req.getContextPath() + "/post/posts.jsp");
+    }
+}
--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -7,10 +7,13 @@ import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Store {
 
     private static final Store INST = new Store();
+
+    private static final AtomicInteger POST_ID = new AtomicInteger(4);
 
     private Map<Integer, Post> posts = new ConcurrentHashMap<>();
     private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
@@ -34,5 +37,10 @@ public class Store {
 
     public Collection<Candidate> findAllCandidates() {
         return candidates.values();
+    }
+
+    public void save(Post post) {
+        post.setId(POST_ID.incrementAndGet());
+        posts.put(post.getId(), post);
     }
 }
